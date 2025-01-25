827c527c0e2a0b6842a9c00011c04896d16dadc4
Diemorth <igorf@dcc.ufba.br>
2025-01-25 08:18:34 -0300

    Dotween and trajectory fix

--- Assets/TrajectoryPointList.cs
+++ Assets/TrajectoryPointList.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class TrajectoryPointList : MonoBehaviour
 {
-    [SerializeField] List<Transform> points = new List<Transform>();
+    [SerializeField] Vector3[] points;
     [SerializeField] LineRenderer line;
+    [SerializeField] Transform target;
+    [SerializeField] float duration = 3f;
+    [SerializeField] Ease ease = Ease.Linear;
     void Start()
     {
         var _index = 0;
+        points = new Vector3[transform.childCount * 2];
         foreach(Transform child in transform) {
-            points.Add(child);
+            points[_index] = child.position;
+            points[(transform.childCount * 2) - _index - 1] = child.position;
             line.positionCount = _index + 1;
             line.SetPosition(_index++, child.position);
         }
+        target.position = points[0];
+        target.DOPath(points, duration).SetEase(ease).SetLoops(-1);
     }
 }
